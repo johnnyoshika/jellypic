5ce21312786a6a064c3e64f3ae3a0874a91c6a7b
Johnny Oshika <johnnyoshika@gmail.com>
2017-05-26 07:56:18 -0700

    Add JellypicContext.

---
+++ Jellypic.Web/Models/JellypicContext.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Jellypic.Web.Models
+{
+    public class JellypicContext : DbContext
+    {
+        public JellypicContext(DbContextOptions<JellypicContext> options) : base(options)
+        {
+        }
+
+        public DbSet<User> Users { get; set; }
+        public DbSet<Post> Posts { get; set; }
+        public DbSet<Like> Likes { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+    }
+
+    public class User
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string ProfilePictureUrl { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public DateTime LastLoggedInAt { get; set; }
+        public int LoginCount { get; set; }
+
+        public List<Post> Posts { get; set; }
+        public List<Like> Likes { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+
+    public class Post
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string ImageUrl { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public List<Like> Likes { get; set; }
+        public List<Comment> Comments { get; set; }
+    }
+
+    public class Like
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
+
+    public class Comment
+    {
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public string Text { get; set; }
+
+        public int UserId { get; set; }
+        public User User { get; set; }
+
+        public int PostId { get; set; }
+        public Post Post { get; set; }
+    }
+}
