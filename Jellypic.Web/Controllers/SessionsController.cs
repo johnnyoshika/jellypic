7393e5e49779e79eafc89ab656e725b70afb5aed
Johnny Oshika <johnnyoshika@gmail.com>
2017-05-26 07:19:19 -0700

    Set up first controller.

---
+++ Jellypic.Web/Controllers/SessionsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jellypic.Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class SessionsController : Controller
+    {
+        [HttpGet("me")]
+        public IActionResult Get()
+        {
+            return Content("Hello world!");
+        }
+    }
+}
