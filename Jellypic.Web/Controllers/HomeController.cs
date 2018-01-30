1ea4a2bd6621ebd7855e4fb5b3f8cc49dd7e57c7
Johnny Oshika <johnnyoshika@gmail.com>
2018-01-30 07:35:20 -0800

    Add HomeController so that NotFoundException won't be thrown when debugging.

---
+++ Jellypic.Web/Controllers/HomeController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Jellypic.Web.Controllers
+{
+    [Route("")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index() =>
+            Content($"Jellypic: {DateTime.Now.ToString("t")}");
+
+        [HttpGet("favicon.ico")]
+        public IActionResult Favicon() =>
+            File(Convert.FromBase64String(
+                "iVBORw0KGgoAAAANSUhEUgAAABAAAAAQEAYAAABPYyMiAAAABmJLR0T///////8JWPfcAAAACXBIWXMAAABIAAAASABGyWs+AAAAF0lEQVRIx2NgGAWjYBSMglEwCkbBSAcACBAAAeaR9cIAAAAASUVORK5CYII="),
+                "image/x-icon");
+    }
+}
