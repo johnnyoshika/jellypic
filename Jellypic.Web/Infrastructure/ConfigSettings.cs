6e967d25d2d37303ffd6b498ffd2c8dd3e7d8a00
Johnny Oshika <johnnyoshika@gmail.com>
2018-01-31 11:56:58 -0800

    Add WebPushVapidKeys in settings.

--- Jellypic.Web/Infrastructure/ConfigSettings.cs
+++ Jellypic.Web/Infrastructure/ConfigSettings.cs
@@ -13,9 +13,11 @@ namespace Jellypic.Web
         public ConfigSettings(IConfigurationRoot root)
         {
             Cloudinary = new CloudinarySettings(root.GetSection("Cloudinary"));
+            WebPushVapidKeys = new WebPushVapidKeys(root.GetSection("WebPushVapidKeys"));
         }
-        
+
         public CloudinarySettings Cloudinary { get; }
+        public WebPushVapidKeys WebPushVapidKeys { get; set; }
     }
 
     public class CloudinarySettings
@@ -26,5 +28,17 @@ namespace Jellypic.Web
         }
 
         public string CloudName { get; }
+    }
+
+    public class WebPushVapidKeys
+    {
+        public WebPushVapidKeys(IConfigurationSection section)
+        {
+            PublicKey = section["PublicKey"];
+            PrivateKey = section["PrivateKey"];
+        }
+
+        public string PublicKey { get; }
+        public string PrivateKey { get; }
     }
 }
