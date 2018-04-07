698db213521586c2e6cff12c94eb9ac35fd73806
Johnny Oshika <johnnyoshika@gmail.com>
2018-04-06 23:47:59 -0700

    Fix NoCacheMiddleware.

--- Jellypic.Web/Infrastructure/NoCacheMiddleware.cs
+++ Jellypic.Web/Infrastructure/NoCacheMiddleware.cs
@@ -19,17 +19,16 @@ namespace Jellypic.Web.Infrastructure
 
         public async Task Invoke(HttpContext httpContext)
         {
-            await Next(httpContext);
-
-            // If response 'has started', then it's most likely that an exception was thrown (e.g. NotFoundException for 404 response).
-            // if response has started, then trying to change the response will throw an exception, but regardless, we shouldn't be setting cache control headers in this situation anyways.
-            if (!httpContext.Response.HasStarted && Options.Paths.Contains(httpContext.Request.Path.Value))
+            // Can't set headers after 'Next' as the response will have already started
+            if (Options.Paths.Contains(httpContext.Request.Path.Value))
             {
                 // https://stackoverflow.com/a/2068407/188740
                 httpContext.Response.Headers.Add("Cache-Control", "no-cache, no-store, must-revalidate");
                 httpContext.Response.Headers.Add("Pragma", "no-cache");
                 httpContext.Response.Headers.Add("Expires", "0");
             }
+
+            await Next(httpContext);
         }
     }
 
