0af7bb95a2b56dd4251522cdcede1689550c70d9
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-22 09:10:58 -0800

    Set up initial GraphQL schema and query

---
+++ Jellypic.Web/GraphQL/JellypicQuery.cs
@@ -0,0 +1,36 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using Jellypic.Web.GraphQL.Types;
+using Jellypic.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jellypic.Web.GraphQL
+{
+    public class JellypicQuery : ObjectGraphType
+    {
+        public JellypicQuery(JellypicContext dataContext)
+        {
+            Field<UserType>(
+                "user",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<IdGraphType>>
+                    {
+                        Name = "id"
+                    }),
+                resolve: context =>
+                {
+                    int id = context.GetArgument<int>("id");
+                    if (id < 1)
+                        context.Errors.Add(new ExecutionError("'id' out of range."));
+
+                    return dataContext
+                        .Users
+                        .FirstOrDefaultAsync(u => u.Id == id);
+                });
+        }
+    }
+}
---
+++ Jellypic.Web/GraphQL/JellypicSchema.cs
@@ -0,0 +1,17 @@
+﻿using GraphQL;
+using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jellypic.Web.GraphQL
+{
+    public class JellypicSchema : Schema
+    {
+        public JellypicSchema(IDependencyResolver resolver) : base(resolver)
+        {
+            Query = resolver.Resolve<JellypicQuery>();
+        }
+    }
+}
