984b1f94ceeb35200ce721b9b400b00510f64c04
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-28 22:43:04 -0800

    Add createPost mutation

---
+++ Jellypic.Web/GraphQL/Inputs/CreatePostInput.cs
@@ -0,0 +1,22 @@
+﻿using GraphQL.Types;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jellypic.Web.GraphQL.Inputs
+{
+    public class CreatePostInput
+    {
+        public string CloudinaryPublicId { get; set; }
+    }
+
+    public class CreatePostInputType : InputObjectGraphType
+    {
+        public CreatePostInputType()
+        {
+            Name = "CreatePostInput";
+            Field<NonNullGraphType<StringGraphType>>("cloudinaryPublicId");
+        }
+    }
+}
--- Jellypic.Web/GraphQL/JellypicMutation.cs
+++ Jellypic.Web/GraphQL/JellypicMutation.cs
@@ -1,5 +1,8 @@
 ﻿using GraphQL.Types;
+using Jellypic.Web.GraphQL.Inputs;
 using Jellypic.Web.GraphQL.Types;
+using Jellypic.Web.Infrastructure;
+using Jellypic.Web.Models;
 using Jellypic.Web.Services;
 using System;
 using System.Collections.Generic;
@@ -10,7 +13,7 @@ namespace Jellypic.Web.GraphQL
 {
     public class JellypicMutation : ObjectGraphType
     {
-        public JellypicMutation(IUserLogin userLogin)
+        public JellypicMutation(IUserLogin userLogin, IUserContext userContext, Func<JellypicContext> dataContext)
         {
             FieldAsync<UserType>(
                 "login",
@@ -20,6 +23,29 @@ namespace Jellypic.Web.GraphQL
                 {
                     string accessToken = context.GetArgument<string>("accessToken");
                     return await userLogin.LogInAsync(accessToken);
+                });
+
+            FieldAsync<NonNullGraphType<ListGraphType<NonNullGraphType<PostType>>>>(
+                "createPost",
+                arguments: new QueryArguments(
+                    new QueryArgument<NonNullGraphType<ListGraphType<NonNullGraphType<CreatePostInputType>>>> { Name = "inputs" }),
+                resolve: async context =>
+                {
+                    var inputs = context.GetArgument<List<CreatePostInput>>("inputs");
+                    var posts = inputs.Select(i => new Post
+                    {
+                        CreatedAt = DateTime.UtcNow,
+                        CloudinaryPublicId = i.CloudinaryPublicId,
+                        UserId = userContext.UserId
+                    }).ToList(); // project to list, otherwise Id won't be set on SaveChangesAsync()
+
+                    using (var dc = dataContext())
+                    {
+                        dc.Posts.AddRange(posts);
+                        await dc.SaveChangesAsync();
+
+                        return posts;
+                    }
                 });
         }
     }
