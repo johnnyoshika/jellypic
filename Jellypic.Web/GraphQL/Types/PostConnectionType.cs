5961f4f2297d223b8b197e88f17a3120c6ea7fab
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-30 09:10:36 -0800

    PostConnectionType inherits from ConnectionType

--- Jellypic.Web/GraphQL/Types/PostConnectionType.cs
+++ Jellypic.Web/GraphQL/Types/PostConnectionType.cs
@@ -7,19 +7,8 @@ using System.Threading.Tasks;
 
 namespace Jellypic.Web.GraphQL.Types
 {
-    public class PostConnectionType : ObjectGraphType<PostConnection>
+    public class PostConnectionType : ConnectionType<PostConnection, Post, PostType>
     {
-        public PostConnectionType()
-        {
-            Name = "PostConnection";
-
-            Field<NonNullGraphType<ListGraphType<NonNullGraphType<PostType>>>>(
-                "nodes",
-                resolve: context => context.Source.Nodes);
-
-            Field<NonNullGraphType<PageInfoType>>(
-                "pageInfo",
-                resolve: context => context.Source.PageInfo);
-        }
+        public override string TypeName => "PostConnection";
     }
 }
