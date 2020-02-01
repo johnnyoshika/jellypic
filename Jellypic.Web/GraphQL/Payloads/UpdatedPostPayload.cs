2f87212b02ad72382789700462fa661556ddc610
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-31 17:17:28 -0800

    Add PostUpdatedSubscriptionService

---
+++ Jellypic.Web/GraphQL/Payloads/UpdatedPostPayload.cs
@@ -0,0 +1,24 @@
+﻿using GraphQL.Types;
+using Jellypic.Web.GraphQL.Types;
+using Jellypic.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jellypic.Web.GraphQL.Payloads
+{
+    public class UpdatedPostPayload
+    {
+        public Post Subject { get; set; }
+    }
+
+    public class UpdatedPostPayloadType : ObjectGraphType<UpdatedPostPayload>
+    {
+        public UpdatedPostPayloadType()
+        {
+            Name = "UpdatedPostPayload";
+            Field(t => t.Subject, type: typeof(NonNullGraphType<PostType>));
+        }
+    }
+}
---
+++ Jellypic.Web/GraphQL/Subscriptions/PostUpdatedSubscriptionService.cs
@@ -0,0 +1,19 @@
+﻿using Jellypic.Web.GraphQL.Payloads;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Reactive.Subjects;
+using System.Threading.Tasks;
+
+namespace Jellypic.Web.GraphQL.Subscriptions
+{
+    public class PostUpdatedSubscriptionService
+    {
+        readonly ISubject<UpdatedPostPayload> _postStream = new ReplaySubject<UpdatedPostPayload>(1);
+
+        public void Notify(UpdatedPostPayload payload) => _postStream.OnNext(payload); // Will send message to all subscribers
+
+        public IObservable<UpdatedPostPayload> GetPost() => _postStream.AsObservable();
+    }
+}
