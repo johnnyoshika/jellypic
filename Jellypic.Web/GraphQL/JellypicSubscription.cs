41352902abb43f2e6ed5c2c91fd7a6e526a60940
Johnny Oshika <johnnyoshika@gmail.com>
2020-01-31 17:30:37 -0800

    Create postUpdated subscription

--- Jellypic.Web/GraphQL/JellypicSubscription.cs
+++ Jellypic.Web/GraphQL/JellypicSubscription.cs
@@ -6,13 +6,14 @@ using Jellypic.Web.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Threading.Tasks;
 
 namespace Jellypic.Web.GraphQL
 {
     public class JellypicSubscription : ObjectGraphType
     {
-        public JellypicSubscription(PostsAddedSubscriptionService postsAddedSubscription)
+        public JellypicSubscription(PostsAddedSubscriptionService postsAddedSubscription, PostUpdatedSubscriptionService postUpdatedSubscription)
         {
             AddField(new EventStreamFieldType
             {
@@ -20,6 +21,20 @@ namespace Jellypic.Web.GraphQL
                 Type = typeof(NonNullGraphType<ListGraphType<NonNullGraphType<AddPostPayloadType>>>),
                 Resolver = new FuncFieldResolver<List<AddPostPayload>>(context => context.Source as List<AddPostPayload>),
                 Subscriber = new EventStreamResolver<List<AddPostPayload>>(context => postsAddedSubscription.GetPosts())
+            });
+
+            AddField(new EventStreamFieldType
+            {
+                Name = "postUpdated",
+                Arguments = new QueryArguments(
+                    new QueryArgument<NonNullGraphType<IdGraphType>> { Name = "id" }),
+                Type = typeof(NonNullGraphType<UpdatedPostPayloadType>),
+                Resolver = new FuncFieldResolver<UpdatedPostPayload>(context => context.Source as UpdatedPostPayload),
+                Subscriber = new EventStreamResolver<UpdatedPostPayload>(context =>
+                {
+                    var id = context.GetArgument<int>("id");
+                    return postUpdatedSubscription.GetPost().Where(p => p.Subject.Id == id);
+                })
             });
         }
     }
