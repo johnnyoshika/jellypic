ebe797f489dc71be0615eea8cfceebf0fc200d44
Johnny Oshika <johnnyoshika@gmail.com>
2020-03-20 09:21:57 -0700

    Add Auth0Login

---
+++ Jellypic.Web/Services/Auth0Login.cs
@@ -0,0 +1,63 @@
+﻿using Jellypic.Web.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Jellypic.Web.Services
+{
+    public class Auth0Login : IUserLogin
+    {
+        public Auth0Login(Func<JellypicContext> dataContext, ISignIn signIn, IAuth0TokenReader tokenReader)
+        {
+            DataContext = dataContext;
+            SignIn = signIn;
+            TokenReader = tokenReader;
+        }
+
+        Func<JellypicContext> DataContext { get; }
+        ISignIn SignIn { get; }
+        IAuth0TokenReader TokenReader { get; }
+
+        public async Task<User> LogInAsync(string token)
+        {
+            var user = await UpsertAsync(await TokenReader.ReadAsync(token));
+            await SignIn.SignInAsync(user.Id.ToString());
+            return user;
+        }
+
+        async Task<User> UpsertAsync(JwtSecurityToken securityToken)
+        {
+            string auth0UserId = securityToken.Claims.First(c => c.Type == "sub").Value;
+            using (var dc = DataContext())
+            {
+                var user = await dc.Users.FirstOrDefaultAsync(u => u.AuthType == "Auth0" && u.AuthUserId == auth0UserId);
+                if (user == null)
+                {
+                    user = new User();
+                    user.CreatedAt = DateTime.UtcNow;
+                }
+
+                user.Username = securityToken.Claims.First(c => c.Type == "nickname").Value;
+                user.AuthType = "Auth0";
+                user.AuthUserId = auth0UserId;
+                user.FirstName = securityToken.Claims.First(c => c.Type == "name").Value;
+                user.Gravatar = UrlWithoutQuery(securityToken.Claims.FirstOrDefault(c => c.Type == "picture")?.Value);
+                user.LastActivityAt = DateTime.UtcNow;
+                user.LastLoggedInAt = DateTime.UtcNow;
+                user.ActivityCount++;
+                user.LoginCount++;
+
+                if (user.Id == 0)
+                    dc.Users.Add(user);
+
+                await dc.SaveChangesAsync();
+                return user;
+            }
+        }
+
+        string UrlWithoutQuery(string url) => string.IsNullOrWhiteSpace(url) ? null : new Uri(url).GetLeftPart(UriPartial.Path);
+    }
+}
